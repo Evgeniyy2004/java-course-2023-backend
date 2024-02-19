653a26f7b78a53e134f8636378c86fddd3439f91
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-02-19 21:30:19 +0500

    working with wiremock

--- scrapper/src/main/java/edu/java/clients/RepoResponse.java
+++ scrapper/src/main/java/edu/java/clients/RepoResponse.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.OffsetDateTime;
 
 public class RepoResponse {
-    @JsonProperty("login") public String owner;
+    public record Owner(@JsonProperty("login") String user){
+    };
+    public Owner owner;
     @JsonProperty("updated_at") public OffsetDateTime time;
     @JsonProperty("name") public String name;
     //@JsonProperty("html_url") public String link;
