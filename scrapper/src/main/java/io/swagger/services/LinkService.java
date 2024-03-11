a54dc27bb2cea49ba73d8d684322bdb852a2579f
evgen <evgeniy.ziganshin.04@mail.ru>
2024-03-11 14:49:49 +0500

    cant push

--- scrapper/src/main/java/io/swagger/services/LinkService.java
+++ scrapper/src/main/java/io/swagger/services/LinkService.java
@@ -6,5 +6,6 @@ import java.util.Collection;
 public interface LinkService {
     void add(long tgChatId, URI url);
     void remove(long tgChatId, URI url);
+
     Collection<URI> listAll(long tgChatId);
 }
