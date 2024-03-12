e590e9b5f0a8725a6ae2c204cd1ea7cde845db13
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-03-12 23:58:23 +0500

    тяжело

--- migrations/changelog.sql
+++ migrations/changelog.sql
@@ -1,19 +1,19 @@
 --liquibase formatted sql
 --changeset evgen:1
 
-create table id
+create table if not exists id
 (
     id              bigint generated always as identity,
     primary key (id)
 );
 
-create table links
+create table  if not exists links
 (
     link              text  not null,
     primary key (link)
 );
 
-create table connect
+create table  if not exists connect
 (
     link              text    not null,
     id                bigint generated always as identity,
