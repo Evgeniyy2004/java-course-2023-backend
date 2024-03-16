f69c6faf8b84d53bdbf6f2c6d7a8696aa367136c
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-03-16 17:12:29 +0500

    need testing

--- scrapper/src/main/java/listener/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/listener/LinkUpdaterScheduler.java
@@ -2,6 +2,7 @@ package listener;
 
 import edu.java.botclient.UpdatesClient;
 import io.swagger.api.LinkRepository;
+import io.swagger.model.LinkUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,7 +22,12 @@ public class LinkUpdaterScheduler {
     public void update() {
         var allchanges = repo.update();
         for (Long id : allchanges.keySet()) {
-
+            var request = new LinkUpdate();
+            request.addTgChatIdsItem(id);
+            for (String url: allchanges.get(id)) {
+                request.setUrl(url);
+                client.post(request);
+            }
         }
     }
 }
