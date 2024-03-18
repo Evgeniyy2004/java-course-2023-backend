f39d70fabb8d77655497e5b2947a10164ce81bce
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-03-18 21:05:05 +0500

    i think i fixed bot but looks like github will fail again

--- bot/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ bot/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -2,13 +2,17 @@ package edu.java.configuration;
 
 import edu.java.scrapperclient.ScrapperChatClient;
 import edu.java.scrapperclient.ScrapperLinksClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+@Validated
 @Configuration
+@ConfigurationProperties(prefix = "app1", ignoreUnknownFields = false)
 public class ClientConfiguration {
     String base = "http://localhost:8080/";
 
