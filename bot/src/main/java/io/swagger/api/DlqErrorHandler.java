12f6d1700c84b6faf82685e1bc454c1974831903
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-04-04 16:25:52 +0500

    checkstyled

--- bot/src/main/java/io/swagger/api/DlqErrorHandler.java
+++ bot/src/main/java/io/swagger/api/DlqErrorHandler.java
@@ -1,16 +1,15 @@
 package io.swagger.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DlqErrorHandler {
     @Autowired
     KafkaTemplate<Integer, String> template;
-        public void handle(String update){
-            template.send("2","Invalid message: " + update);
-        }
+
+    public void handle(String update) {
+        template.send("2", "Invalid message: " + update);
+    }
 }
