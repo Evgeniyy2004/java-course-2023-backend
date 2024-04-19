bca74781f644821b0f176e4bf95dfe5e457be6d9
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-04-19 10:08:38 +0500

    checkstyled

--- bot/src/main/java/io/swagger/api/DlqErrorHandler.java
+++ bot/src/main/java/io/swagger/api/DlqErrorHandler.java
@@ -1,6 +1,5 @@
 package io.swagger.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.core.KafkaTemplate;
