dbbe5f3d1d2d4569b9e46e67eb8e924b9659c679
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-04-19 10:16:00 +0500

    mda not checkstyled

--- scrapper/src/main/java/io/swagger/api/ScrapperQueueProducer.java
+++ scrapper/src/main/java/io/swagger/api/ScrapperQueueProducer.java
@@ -2,21 +2,17 @@ package io.swagger.api;
 
 import edu.java.configuration.ApplicationConfig;
 import edu.java.model.LinkUpdate;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScrapperQueueProducer {
 
-
     private final KafkaTemplate<Integer, String> template;
 
     public ScrapperQueueProducer(KafkaTemplate<Integer, String> template) {
         this.template = template;
     }
-
 
     public void send(LinkUpdate update) {
         // TODO
