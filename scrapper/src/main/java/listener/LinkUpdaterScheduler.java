68fa5a03cb4ea04439e3f5b7c149a0ad5ec0b8cc
Evgeniy Zig <evgeniy.ziganshin.04@mail.ru>
2024-04-26 23:35:40 +0500

    making jpa work like jpa

--- scrapper/src/main/java/listener/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/listener/LinkUpdaterScheduler.java
@@ -31,7 +31,9 @@ public class LinkUpdaterScheduler {
         JPA, JDBC
     }
 
-    @Value()
+    @Value("scheduler.use")
+    private AccessType type;
+
 
     @Autowired
     private UpdatesClient client;
@@ -51,41 +53,17 @@ public class LinkUpdaterScheduler {
     @Scheduled(fixedDelayString = "86400s")
     public void update() {
         var time = new Timestamp(System.currentTimeMillis() - 3600000);
-        List<LinkResponse> res;
-        var res = repo1.findByTime(time);
-        HashMap<Long, Collection<String>> result = new HashMap<>();
-        for (Object i : res) {
-            var list = (ArrayList) i;
-            var current = list.get(1).toString();
-            var id = Long.parseLong(list.get(0).toString());
-            if (current.startsWith("https://stackoverflow.com/questions/")) {
-                current = current.replace("https://stackoverflow.com/questions/", "");
-                var question = Long.parseLong(current.split("/")[0]);
-                var response = stack.fetchQuestion(question);
-                if (Timestamp.valueOf(response.time.toLocalDateTime()).after((Timestamp) list.get(2))) {
-                    if (!result.containsKey(id)) {
-                        result.put(id, new ArrayList<>());
-                    }
-                    result.get(id).add(list.get(1).toString());
+        if (type == AccessType.JPA) {
+            var res = repo1.findByTime(time);
+        } else {
+            var result = repo.update();
+            for (Long id : result.keySet()) {
+                var request = new LinkUpdate();
+                request.setId(id);
+                for (String url : result.get(id)) {
+                    request.setUrl(url);
+                    client.post(request);
                 }
-            } else {
-                current = current.replace("https://github.com/", "");
-                var repoAuthor = current.split("/");
-                var response = git.fetchRepository(repoAuthor[0], repoAuthor[1]);
-                if (Timestamp.valueOf(response.time.toLocalDateTime()).after((Timestamp) list.get(2))) {
-                    if (!result.containsKey(id)) {
-                        result.put(id, new ArrayList<>());
-                    }
-                    result.get(id).add(list.get(1).toString());
-                }
-            }
-        }
-        for (Long id : result.keySet()) {
-            var request = new LinkUpdate();
-            request.setId(id);
-            for (String url : result.get(id)) {
-                request.setUrl(url);
-                client.post(request);
             }
         }
     }
